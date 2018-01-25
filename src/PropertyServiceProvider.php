8abf97cd0c457fad4dce8f4122f2098139e5f729
Tam Nguyen <tam@15gl.org>
2018-01-25 22:04:55 +0700

    add route

---
+++ src/PropertyServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Shuramita\Property\Controllers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Routing\Router;
+
+class PropertyServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot(Router $router)
+    {
+        $this->loadRoutesFrom(__DIR__.'/routes.php');
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(__DIR__.'/config/property.php', 'property');
+    }
+}
